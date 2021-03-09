d368d7d3f08651ebd42d763c11d6dffe1a905b98
mali.tian <mali.tian@rea-group.com>
2021-03-09 12:33:36 +0800

    Add the playing bar on index page

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include("includes/config.php");
 // session_destroy(); // this is for logout
 
 if (isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
+  $userLoggedIn = $_SESSION['userLoggedIn'];
 } else {
-	header("Location: register.php");
+  header("Location: register.php");
 }
 
 ?>
@@ -16,10 +16,47 @@ if (isset($_SESSION['userLoggedIn'])) {
 
 <head>
   <title>Hello</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
-  Hello world!!!
+
+  <div id="nowPlayingContainer">
+    <div id="nowPlayingBar">
+      <div id="nowPlayingLeft"></div>
+      <div id="nowPlayingCenter">
+
+        <div class="content playerControls">
+          <div class="buttons">
+            <button class="controlButton shuffle" title="Shuffle button">
+              <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+            </button>
+
+            <button class="controlButton previous" title="Previous button">
+              <img src="assets/images/icons/previous.png" alt="Previous">
+            </button>
+
+            <button class="controlButton play" title="Play button">
+              <img src="assets/images/icons/play.png" alt="Play">
+            </button>
+
+            <button class="controlButton pause" title="Pause button" style="display: none;">
+              <img src="assets/images/icons/pause.png" alt="Pause">
+            </button>
+
+            <button class="controlButton next" title="Next button">
+              <img src="assets/images/icons/next.png" alt="Next">
+            </button>
+
+            <button class="controlButton repeat" title="Repeat button">
+              <img src="assets/images/icons/repeat.png" alt="Repeat">
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="nowPlayingRight"></div>
+    </div>
+  </div>
 </body>
 
 </html>
