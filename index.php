fa423bc41b945cac3646aef5e6e40eec9a6faed4
mali.tian <mali.tian@rea-group.com>
2021-03-09 14:20:28 +0800

    Volume bar section

--- index.php
+++ index.php
@@ -78,7 +78,18 @@ if (isset($_SESSION['userLoggedIn'])) {
           </div>
         </div>
       </div>
-      <div id="nowPlayingRight"></div>
+      <div id="nowPlayingRight">
+        <div class="volumeBar">
+          <button class="controlButton volume" title="Volume Button">
+            <img src="assets/images/icons/volume.png" alt="Volume">
+          </button>
+          <div class="progressBar">
+            <div class="progressBarBg">
+              <div class="progress"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </body>
